f3832af7e627a5d211eb9a52f625ad177d59d3f0
Gopise <gopisey@marvell.com>
2013-08-10 15:57:58 +0800

    fix backlight adjust issue and private version number increased

--- liblight/lights.c
+++ liblight/lights.c
@@ -50,7 +50,7 @@ static struct light_state_t g_notification;
 static int g_charge_led_active;
 static int g_last_button_brightness;
 
-char const*const LCD_FILE = "/sys/class/backlight/430_540_960_amoled_bl/brightness";
+char const*const LCD_FILE = "/sys/class/backlight/lm3532_bl/brightness";
 char const*const BUTTON_ON_FILE = "/sys/class/leds/button-backlight/brightness";
 
 /* RGB file descriptors */
